3d48ace369333c5b09f7484560f7c4be32dbf107
anas salagar <alisalagarf@gmail.com>
2021-11-21 16:08:40 +0530

    [anas UC7 for ensure duplicate entry using lambda]

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,40 +6,53 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            string keyPress = "o";
             //Display welcome message 
             Console.WriteLine("Welcome to the Address Book program");
             Console.WriteLine();
-            Console.WriteLine("Select the below option");
-            //Provideing option for user
-            Console.WriteLine("1 : Add Contact Details \n2 : View Contact Details \n3 : Edit Contact Details \n4 : Delete Contact Details");
-            Console.WriteLine();
-            int num = Convert.ToInt32(Console.ReadLine());
-            //Creating object of AddressBook class
-            AddressBook obj = new AddressBook();
-
-            while (keyPress != "n")
+            //taking input from user 
+            Console.Write("Enter number of Address Book you want : ");
+            int numBook = Convert.ToInt32(Console.ReadLine());
+            int numberBook = 0;
+            string keyPress = "o";
+            while (numberBook < numBook)
             {
-                //switch statement for show method as per user choose option
-                switch (num)
+                //taking address book name from user
+                Console.WriteLine("Enter name of adress book");
+                string book = Console.ReadLine();
+                Console.WriteLine("Select the below option");
+                //Provideing option for user
+                Console.WriteLine("1 : Add Contact Details \n2 : View Contact Details \n3 : Edit Contact Details \n4 : Delete Contact Details");
+                Console.WriteLine();
+                int num = Convert.ToInt32(Console.ReadLine());
+                //Creating object of AddressBook class
+                AddressBook obj = new AddressBook();
+
+                while (keyPress != "n")
                 {
-                    case 1:
-                        obj.AddContact();
-                        break;
+                    //switch statement for show method as per user choose option
+                    switch (num)
+                    {
+                        case 1:
+                            obj.AddContact();
+                            break;
 
-                    case 2:
-                        obj.ViewDetails();
-                        break;
+                        case 2:
+                            obj.ViewDetails();
+                            break;
 
-                    case 3:
-                        obj.EditDetails();
-                        break;
-                    case 4:
-                        obj.DeleteContact();
-                        break;
+                        case 3:
+                            obj.EditDetails();
+                            break;
+                        case 4:
+                            obj.DeleteContact();
+                            break;
+                    }
+                    Console.WriteLine("Do you wish to continue? Press (y/n)");
+                    keyPress = Console.ReadLine();
                 }
-                Console.WriteLine("Do you wish to continue? Press (y/n)");
-                keyPress = Console.ReadLine();
+                //calling method to add new addressbook in dictionary
+                AddressBook.AddTo(book);
+                numberBook++;
             }
         }
     }
